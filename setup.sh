8c5a2eacdd681912e45fb9a3839b4d43fe14043f
Marc Giestin Louis Cordova <154757927+cordy001@users.noreply.github.com>
2025-05-17 00:59:33 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,19 @@
 clear
 
 echo " "
-echo "++++++++++++++++++++++++++++++"
-echo "Starting Set Up Kali Termux Automation by Cordy"
-echo "±+++++++++++++++++++++++++++++"
+
+echo "
+CCCCCC   OOOOOO   RRRRRR    DDDD    YY    YY
+CC       OO  OO   RR  RR    DD  DD   YY  YY  
+CC       OO  OO   RRRRR     DD  DD     YY
+CCCCCC   OOOOOO   RR   RR   DDDD       YY
+"
+
+echo " "
+echo "##############################"
+echo "Starting Set Up NetHunter Termux"
+echo "Automation tools by Cordy"
+echo "±+++++++++++++++++++++++++++++Beta 0.1124.03"
 echo " "
 
 apt update && apt upgrade -y
@@ -12,36 +22,55 @@ apt update && apt upgrade -y
 termux-setup-storage
 
 echo " "
-echo "++++++++++++++++++++++++++++++"
+echo "##############################"
 echo "Extracting NetHunter Fragments"
-echo "±+++++++++++++++++++++++++++++"
+echo "##############################"
 echo " "
 
 cd $HOME/TermuxNetHunter
 
+FILE="kali-nethunter-daily-dev-rootfs-full-arm64.tar.xz"
+
+if [ -f "$FILE" ]; then
+
+echo "File Already Exist"
+
+else 
+
 apt install p7zip -y
 
-7z x NHfragments/kali-nethunter_fragments.zip.001 -o$HOME/TermuxNetHunter
+7z x NHfragments/kali-nethunter_fragments.zip.001 -o"$HOME/"
+
+fi
+
+
 
 echo " "
-echo "++++++++++++++++++++++++++++++"
+echo "##############################"
 echo "Installing Wget"
-echo "±+++++++++++++++++++++++++++++"
+echo "##############################"
 echo " "
 
 apt install wget -y
 
 echo " "
-echo "++++++++++++++++++++++++++++++"
+echo "##############################"
 echo "Installing Net Hunter Rootless"
-echo "±+++++++++++++++++++++++++++++"
+echo "##############################"
 echo " "
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
 
 chmod +x install-nethunter-termux
 
+echo "
+-------
+Don’t forget to follow me on Tiktok for more automation tools I will make in the future🧑‍💻🧑‍💻
+-------
+"
+
 echo " "
-echo "This will Open the NetHunter Installation Packages click Enter"
+echo "++++++++++++++++++++++++++++++"
+echo "Click enter to open nethunter installer"
 echo " "
 read
 ./install-nethunter-termux
