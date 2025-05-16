7b80089ffc77f9c35a6023bfaafc96f09a6d713d
Marc Giestin Louis Cordova <154757927+cordy001@users.noreply.github.com>
2025-05-16 16:15:34 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,33 @@
 
-
+echo " "
 echo "++++++++++++++++++++++++++++++"
-echo "Starting Set Up Kali Termux Automation by CordyPH🇵🇭"
+echo "Starting Set Up Kali Termux Automation by Cordy"
 echo "±+++++++++++++++++++++++++++++"
+echo " "
 
 apt update && apt upgrade -y
 
 termux-setup-storage
 
+echo " "
 echo "++++++++++++++++++++++++++++++"
 echo "Installing Wget"
 echo "±+++++++++++++++++++++++++++++"
+echo " "
 
 apt install wget -y
 
+echo " "
 echo "++++++++++++++++++++++++++++++"
 echo "Installing Net Hunter Rootless"
 echo "±+++++++++++++++++++++++++++++"
-
+echo " "
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
 
 chmod +x install-nethunter-termux
 
 ./install-nethunter-termux
+
+1
+
+n
