a137554262e9445945cdfecadccc9472f1043db9
Marc Giestin Louis Cordova <154757927+cordy001@users.noreply.github.com>
2025-05-16 23:53:01 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,8 @@ echo "Extracting NetHunter Fragments"
 echo "±+++++++++++++++++++++++++++++"
 echo " "
 
+cd $HOME/TermuxNetHunter
+
 apt install p7zip -y
 
 7z x NHfragments/kali-nethunter_fragments.zip.001 -o$HOME/TermuxNetHunter
@@ -38,4 +40,8 @@ wget -O install-nethunter-termux https://offs.ec/2MceZWr
 
 chmod +x install-nethunter-termux
 
+echo " "
+echo "This will Open the NetHunter Installation Packages click Enter"
+echo " "
+read
 ./install-nethunter-termux
